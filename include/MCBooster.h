2e3d1b0adbeccd6decff41ec22c8fc0edaca0733
Trent Patterson <jtrent238@outlook.com>
2016-05-18 20:36:30 -0400

    Add files via upload

---
+++ include/MCBooster.h
@@ -0,0 +1,16 @@
+#ifndef MCBOOSTER_H
+#define MCBOOSTER_H
+
+
+class MCBooster
+{
+    public:
+        MCBooster();
+        virtual ~MCBooster();
+
+    protected:
+
+    private:
+};
+
+#endif // MCBOOSTER_H
